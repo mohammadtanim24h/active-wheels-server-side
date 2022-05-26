3c49930ff2fa32c376c0c6f01ee96d47ed12a2a0
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-26 20:40:25 +0600

    verify jwt function added

--- index.js
+++ index.js
@@ -14,6 +14,22 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gmkk05y.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// verify jwt
+function verifyJWT (req, res, next) {
+    const authHeader = req.headers.authorization;
+    if(!authHeader) {
+        return res.status(401).send({message: "Unauthorized Access"});
+    }
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+        if(err) {
+            return res.status(403).send({message: "Forbidden Access"});
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
 async function run() {
     try {
         await client.connect();
@@ -59,11 +75,17 @@ async function run() {
         })
 
         // get specific orders with email
-        app.get("/order/:email", async (req, res) => {
+        app.get("/order/:email", verifyJWT, async (req, res) => {
             const email = req.params.email;
-            const query = {email};
-            const orders = await orderCollection.find(query).toArray();
-            res.send(orders)
+            const decodedEmail = req.decoded.email;
+            if(email === decodedEmail) {
+                const query = {email};
+                const orders = await orderCollection.find(query).toArray();
+                return res.send(orders);
+            }
+            else {
+                return res.status(403).send({message: "Forbidden Access"});
+            }
         })
 
         // delete a order
