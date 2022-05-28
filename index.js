7331602407a222e1480ea1bc8704b22f5e413b8b
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-28 20:34:06 +0600

    added jwt verification in order deletion

--- index.js
+++ index.js
@@ -203,7 +203,7 @@ async function run() {
         })
 
         // delete a order
-        app.delete("/order/:id", async (req, res) => {
+        app.delete("/order/:id", verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = {_id: ObjectId(id)};
             const result = await orderCollection.deleteOne(query);
